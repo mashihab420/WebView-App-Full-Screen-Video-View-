9b532d357ba9d91ecb80130295250f5d7d87966a
Shihab <mahbubul15-8821@diu.edu.bd>
2020-06-30 00:18:30 +0600

    WebView with full screen video(done)

--- app/src/main/java/com/codian/animekookiee/MainActivity.java
+++ app/src/main/java/com/codian/animekookiee/MainActivity.java
@@ -161,73 +161,9 @@ public class MainActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(3846);
         }
     }
-}
 
             //exit
 
-    /*private class ChromeClient extends WebChromeClient {
-        private View mCustomView;
-        private WebChromeClient.CustomViewCallback mCustomViewCallback;
-        protected FrameLayout mFullscreenContainer;
-        private int mOriginalOrientation;
-        private int mOriginalSystemUiVisibility;
-
-        ChromeClient() {}
-
-        public Bitmap getDefaultVideoPoster()
-        {
-            if (mCustomView == null) {
-                return null;
-            }
-            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
-        }
-
-        public void onHideCustomView()
-        {
-            ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
-            this.mCustomView = null;
-            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
-            setRequestedOrientation(this.mOriginalOrientation);
-            this.mCustomViewCallback.onCustomViewHidden();
-            this.mCustomViewCallback = null;
-        }
-
-        public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback)
-        {
-            if (this.mCustomView != null)
-            {
-                onHideCustomView();
-                return;
-            }
-            this.mCustomView = paramView;
-            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
-            this.mOriginalOrientation = getRequestedOrientation();
-            this.mCustomViewCallback = paramCustomViewCallback;
-            ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
-            getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
-    }
-
-
-    class Browser_home extends WebViewClient {
-        Browser_home() {
-
-        }
-
-
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            binding.progressBg.setVisibility(View.GONE);
-            binding.progressbar.setVisibility(View.GONE);
-            super.onPageFinished(view, url);
-        }
-    }
 
     @Override
     public void onBackPressed() {
@@ -259,6 +195,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-    }*/
+    }
+
+}
 
 
